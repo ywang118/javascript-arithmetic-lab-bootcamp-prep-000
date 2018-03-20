c813ae36d74550ceef6dd656591a86a917284668
wangfanqoo@126.com Wang <wangfanqoo@126.com>
2018-03-20 20:54:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function add(a, b) {
     return a+b  
-}
+};
 function subtract(a, b){
   return a-b
-}
+};
 function multiply(a, b){
   return a*b
-}
+};
 function divide(a, b){
-  reutnr a/b
-}
+  return a/b
+};
 
 
